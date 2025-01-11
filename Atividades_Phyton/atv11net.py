67102ee3cdd28fa02922baf6218e67c1b14eb1df
Camilli Honorato <camillihonoratovit@gmail.com>
2025-01-11 16:55:02 -0300

    Add files via upload

---
+++ Atividades_Phyton/atv11net.py
@@ -0,0 +1,22 @@
+#Questão: Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#a)O produto do dobro do primeiro com metade do segundo .
+#b)A soma do triplo do primeiro com o terceiro.
+#C)O terceiro elevado ao cubo.
+
+#Camilli Vitória Salvaro Honorato
+#Cursando Curso Técnico em Informática
+#Feito em: 20/09/2024
+
+#Este programa foi feito por disposição própria e não pelo curso. Estou aplicando o conhecimento do curso juntamente
+#com pesquisas online.
+
+n1 = int(input("Informe um numero inteiro: "))
+n2 = int(input("Informe mais um numero inteiro: "))
+r1 = float(input("Informe um numero real: "))
+
+a= (n1 *2)*(n2 / 2)
+b= (n1 * 3)+(r1)
+c= r1 * r1 * r1
+print("O produto do dobro do primeiro numero com metade do segundo: %.2f"%a)
+print("A soma do triplo do primeiro numero com o terceiro: %.2f"%b)
+print("O terceiro numero elevado ao cubo: %.2f"%c)
---
+++ Atividades_Phyton/atv15net.py
@@ -0,0 +1,55 @@
+#Questão: Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+# Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda,
+# 8% para o INSS e 5% para o sindicato, faça um programa que nos dê:
+# - Salário bruto.
+# - Quanto pagou ao INSS.
+# - Quanto pagou ao sindicato.
+# - O salário líquido.
+#Calcule os descontos e o salário líquido, conforme a tabela abaixo:
+# * Salário Bruto : R$
+# * IR (11%) : R$
+# * INSS (8%) : R$
+# * Sindicato ( 5%) : R$
+# * Salário Liquido : R$
+# Obs.: Salário Bruto - Descontos = Salário Líquido.
+
+#Camilli Vitória Salvaro Honorato
+#Cursando Curso Técnico em Informática
+#Feito em: 20/09/2024
+
+#Este programa foi feito por disposição própria e não pelo curso. Estou aplicando o conhecimento do curso juntamente
+#com pesquisas online.
+
+
+ganha_hora = float(input("Quanto você ganha por hora? "))
+hora_mes = float(input("Quantas horas você trabalhou no mês?"))
+
+def salario_bruto():
+    return ganha_hora* hora_mes 
+
+def IR():
+    return salario_bruto() * (11/100)
+
+def INSS():
+    return salario_bruto() * (8/100)
+
+def sindicato():
+    return salario_bruto() * (5/100)
+  
+
+def descontos():
+    return IR() + INSS() + sindicato()
+
+def salario_liquido():
+    return salario_bruto() - descontos()
+
+print("")
+print("---Folha de Pagamento---")
+print("")
+print("- Salario Bruto: R$%.2f" %salario_bruto())
+print("- IR(11%): R$ ", IR())
+print("- INSS(8%): R$ ", INSS())
+print("- Sindicato(5%): R$ ", sindicato())
+print("- Desconto: ", descontos())
+print("- Salario Liquido: R$", salario_liquido())
+print("")
